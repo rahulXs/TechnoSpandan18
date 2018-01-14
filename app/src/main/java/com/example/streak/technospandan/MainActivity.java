a25c57103815e1e2b20d5a0175fd1c7d2853c1fe
rahulXs <rahulsharmaacn@gmail.com>
2018-01-14 10:40:31 +0530

    initial commit

---
+++ app/src/main/java/com/example/streak/technospandan/MainActivity.java
@@ -0,0 +1,120 @@
+package com.example.streak.technospandan;
+
+import android.app.Fragment;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.ColorInt;
+import android.support.annotation.ColorRes;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+
+import com.example.streak.technospandan.fragment.CenteredTextFragment;
+import com.example.streak.technospandan.menu.DrawerAdapter;
+import com.example.streak.technospandan.menu.DrawerItem;
+import com.example.streak.technospandan.menu.SimpleItem;
+import com.example.streak.technospandan.menu.SpaceItem;
+import com.yarolegovich.slidingrootnav.SlidingRootNav;
+import com.yarolegovich.slidingrootnav.SlidingRootNavBuilder;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity implements DrawerAdapter.OnItemSelectedListener {
+
+    private static final int POS_HOME = 0;
+    private static final int POS_TEAM = 1;
+    private static final int POS_NOTIFICATIONS = 2;
+    private static final int POS_SETTINGS = 3;
+    private static final int POS_EXIT = 5;
+
+    private String[] screenTitles;
+    private Drawable[] screenIcons;
+
+    private SlidingRootNav slidingRootNav;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        slidingRootNav = new SlidingRootNavBuilder(this)
+                .withToolbarMenuToggle(toolbar)
+                .withMenuOpened(false)
+                .withContentClickableWhenMenuOpened(false)
+                .withSavedState(savedInstanceState)
+                .withMenuLayout(R.layout.menu_left_drawer)
+                .inject();
+
+        screenIcons = loadScreenIcons();
+        screenTitles = loadScreenTitles();
+
+        DrawerAdapter adapter = new DrawerAdapter(Arrays.asList(
+                createItemFor(POS_HOME).setChecked(true),
+                createItemFor(POS_TEAM),
+                createItemFor(POS_NOTIFICATIONS),
+                createItemFor(POS_SETTINGS),
+                new SpaceItem(48),
+                createItemFor(POS_EXIT)));
+        adapter.setListener(this);
+
+        RecyclerView list = findViewById(R.id.list);
+        list.setNestedScrollingEnabled(false);
+        list.setLayoutManager(new LinearLayoutManager(this));
+        list.setAdapter(adapter);
+
+        adapter.setSelected(POS_HOME);
+    }
+
+    @Override
+    public void onItemSelected(int position) {
+        if (position == POS_EXIT) {
+            finish();
+        }
+        slidingRootNav.closeMenu();
+        Fragment selectedScreen = CenteredTextFragment.createFor(screenTitles[position]);
+        showFragment(selectedScreen);
+    }
+
+    private void showFragment(Fragment fragment) {
+        getFragmentManager().beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit();
+    }
+
+    private DrawerItem createItemFor(int position) {
+        return new SimpleItem(screenIcons[position], screenTitles[position])
+                .withIconTint(color(R.color.textColorSecondary))
+                .withTextTint(color(R.color.textColorPrimary))
+                .withSelectedIconTint(color(R.color.colorAccent))
+                .withSelectedTextTint(color(R.color.colorAccent));
+    }
+
+    private String[] loadScreenTitles() {
+        return getResources().getStringArray(R.array.ld_activityScreenTitles);
+    }
+
+    private Drawable[] loadScreenIcons() {
+        TypedArray ta = getResources().obtainTypedArray(R.array.ld_activityScreenIcons);
+        Drawable[] icons = new Drawable[ta.length()];
+        for (int i = 0; i < ta.length(); i++) {
+            int id = ta.getResourceId(i, 0);
+            if (id != 0) {
+                icons[i] = ContextCompat.getDrawable(this, id);
+            }
+        }
+        ta.recycle();
+        return icons;
+    }
+
+    @ColorInt
+    private int color(@ColorRes int res) {
+        return ContextCompat.getColor(this, res);
+    }
+}
