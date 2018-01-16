7c4b4f227aeb8d1dc352b11e93c52ef7e38029ed
PIYUSH SHARMA <piyushsharma6715@gmail.com>
2018-01-16 23:39:33 +0630

    Commit 4- Home fragment come to live state.

---
+++ app/src/main/java/com/example/streak/technospandan/utils/ViewPagerAdapter.java
@@ -0,0 +1,62 @@
+package com.example.streak.technospandan.utils;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+import com.example.streak.technospandan.R;
+/**
+ * Created by PIYUSH on 16-01-2018.
+ */
+
+
+
+
+public class ViewPagerAdapter extends PagerAdapter {
+
+    private Context context;
+    private LayoutInflater layoutInflater;
+    private Integer [] images = {R.drawable.slide1,R.drawable.slide2,R.drawable.slide3};
+
+    public ViewPagerAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, final int position) {
+
+        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.custom_layout, null);
+        ImageView imageView = (ImageView) view.findViewById(R.id.imageView);
+        imageView.setImageResource(images[position]);
+
+
+        ViewPager vp = (ViewPager) container;
+        vp.addView(view, 0);
+        return view;
+
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+
+        ViewPager vp = (ViewPager) container;
+        View view = (View) object;
+        vp.removeView(view);
+
+    }
+}
