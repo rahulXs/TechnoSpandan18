b9c8d0a71784d00036b97e520784fff697673682
rahulXs <rahulsharmaacn@gmail.com>
2018-01-20 15:06:46 +0530

    Fixed app crashing while navigating between different nav menu; Add Settings activity as Menu item.

--- app/src/main/java/com/example/streak/technospandan/activity/MainActivity.java
+++ app/src/main/java/com/example/streak/technospandan/activity/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.streak.technospandan.activity;
 
-import android.app.Fragment;
-import android.app.FragmentTransaction;
+import android.app.Fragment;;
+import android.content.Intent;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.ColorInt;
@@ -13,10 +13,14 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.streak.technospandan.R;
-import com.example.streak.technospandan.fragment.CenteredTextFragment;
 import com.example.streak.technospandan.fragment.HomeFragment;
+import com.example.streak.technospandan.fragment.NotificationsFragment;
+import com.example.streak.technospandan.fragment.TeamFragment;
 import com.example.streak.technospandan.menu.DrawerAdapter;
 import com.example.streak.technospandan.menu.DrawerItem;
 import com.example.streak.technospandan.menu.SimpleItem;
@@ -26,31 +30,46 @@ import com.yarolegovich.slidingrootnav.SlidingRootNavBuilder;
 
 import java.util.Arrays;
 
+import static android.R.attr.id;
+
 public class MainActivity extends AppCompatActivity implements DrawerAdapter.OnItemSelectedListener {
 
     private static final int POS_HOME = 0;
     private static final int POS_TEAM = 1;
     private static final int POS_NOTIFICATIONS = 2;
-    private static final int POS_SETTINGS = 3;
     private static final int POS_EXIT = 5;
 
     private String[] screenTitles;
     private Drawable[] screenIcons;
 
-
     Fragment fragment=null;
 
-
-
     private SlidingRootNav slidingRootNav;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater= getMenuInflater();
+        inflater.inflate(R.menu.menu_items,menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id=item.getItemId();
+        if(id==R.id.Settings){
+            Intent i=new Intent(this,SettingsActivity.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         slidingRootNav = new SlidingRootNavBuilder(this)
@@ -68,12 +87,11 @@ public class MainActivity extends AppCompatActivity implements DrawerAdapter.OnI
                 createItemFor(POS_HOME).setChecked(true),
                 createItemFor(POS_TEAM),
                 createItemFor(POS_NOTIFICATIONS),
-                createItemFor(POS_SETTINGS),
                 new SpaceItem(48),
                 createItemFor(POS_EXIT)));
         adapter.setListener(this);
 
-        RecyclerView list = findViewById(R.id.list);
+        RecyclerView list = (RecyclerView) findViewById(R.id.list);
         list.setNestedScrollingEnabled(false);
         list.setLayoutManager(new LinearLayoutManager(this));
         list.setAdapter(adapter);
@@ -82,23 +100,45 @@ public class MainActivity extends AppCompatActivity implements DrawerAdapter.OnI
 
     @Override
     public void onItemSelected(int position) {
+
         if (position == POS_EXIT) {
-            HomeFragment f=(HomeFragment)fragment;
-            f.setFlag(1);
+            HomeFragment.setFlag(1);
+            TeamFragment.setFlag(1);
+            NotificationsFragment.setFlag(1);
             finish();
         }
         slidingRootNav.closeMenu();
 
-       if(position==POS_HOME){
-           fragment=HomeFragment.createFor(screenTitles[position]);
-           showFragment(fragment);
-       }
-       else {
-           HomeFragment f=(HomeFragment)fragment;
-           f.setFlag(1);
-           Fragment selectedScreen = CenteredTextFragment.createFor(screenTitles[position]);
-           showFragment(selectedScreen);
-       }
+        if(position==POS_HOME){
+
+           // HomeFragment.setFlag(1);
+            TeamFragment.setFlag(1);
+            NotificationsFragment.setFlag(1);
+            fragment=HomeFragment.createFor(screenTitles[position]);
+            showFragment(fragment);
+        }
+        else if (position==POS_TEAM){
+
+            HomeFragment.setFlag(1);
+            //TeamFragment.setFlag(1);
+            NotificationsFragment.setFlag(1);fragment= TeamFragment.createFor(screenTitles[position]);
+            showFragment(fragment);
+        }
+        else if (position==POS_NOTIFICATIONS){
+
+            HomeFragment.setFlag(1);
+            TeamFragment.setFlag(1);
+            //NotificationsFragment.setFlag(1);
+            fragment= NotificationsFragment.createFor(screenTitles[position]);
+            showFragment(fragment);
+        }
+
+        /*else {
+            SettingsFragment f=(SettingsFragment)fragment;
+            f.setFlag(1);
+            Fragment selectedScreen = CenteredTextFragment.createFor(screenTitles[position]);
+            showFragment(selectedScreen);
+        }*/
     }
 
     private void showFragment(Fragment fragment) {
--- app/src/main/java/com/example/streak/technospandan/fragment/NotificationsFragment.java
+++ app/src/main/java/com/example/streak/technospandan/fragment/NotificationsFragment.java
@@ -1,109 +1,72 @@
 package com.example.streak.technospandan.fragment;
 
-import android.content.Context;
-import android.net.Uri;
+/**
+ * Created by streak on 19/1/18.
+ */
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.streak.technospandan.R;
+import com.example.streak.technospandan.utils.ViewPagerAdapter;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link NotificationsFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link NotificationsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class NotificationsFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    static  int flag=0;                     //to make app not to crash
+    int currentPage = 0;
+    ViewPager viewPager;
 
-    private OnFragmentInteractionListener mListener;
+    private static final String EXTRA_TEXT = "text";
 
-    public NotificationsFragment() {
-        // Required empty public constructor
+    public static void setFlag(int f){
+        flag = f;
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NotificationsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static NotificationsFragment newInstance(String param1, String param2) {
+    public static NotificationsFragment createFor(String text) {
         NotificationsFragment fragment = new NotificationsFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(EXTRA_TEXT, text);
         fragment.setArguments(args);
         return fragment;
     }
 
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_notifications, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        getActivity().setTitle("Notifications");
+        final String text = getArguments().getString(EXTRA_TEXT);
+        TextView textView = view.findViewById(R.id.text);
+        textView.setText(text);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                Toast.makeText(v.getContext(), text, Toast.LENGTH_SHORT).show();
+            }
+        });
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        final Handler handler = new Handler();
+        final Runnable Update = new Runnable() {
+            public void run() {
+                if (currentPage == (new ViewPagerAdapter(getActivity()).getCount())) {
+                    currentPage = 0;
+                }
+                if (flag==0)
+                    viewPager.setCurrentItem(currentPage++, true);
+                else
+                    onDestroyView();
+            }
+        };
     }
 }
