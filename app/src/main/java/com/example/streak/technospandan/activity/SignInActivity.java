de51d2b47e1d9b853ed037f2cc2b7e6a0fc69f8c
rahulXs <rahulsharmaacn@gmail.com>
2018-03-15 15:46:17 +0530

    Minor fixes

--- app/src/main/java/com/example/streak/technospandan/activity/SignInActivity.java
+++ app/src/main/java/com/example/streak/technospandan/activity/SignInActivity.java
@@ -10,16 +10,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.streak.technospandan.R;
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInApi;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.firebase.auth.FirebaseAuth;
+
 
 import es.dmoral.toasty.Toasty;
 
@@ -30,102 +21,20 @@ public class SignInActivity extends AppCompatActivity {
     // TAG is for show some tag logs in LOG screen.
     public static final String TAG = "MainActivity";
 
-    // Request sing in code. Could be anything as you required.
-    public static final int RequestSignInCode = 7;
-
-    // Firebase Auth Object.
-    public FirebaseAuth firebaseAuth;
-
-    // Google API Client object.
-    public GoogleApiClient googleApiClient;
-
-    // Sing out button.
-    Button SignOutButton;
-
-    // Google Sign In button .
-    com.google.android.gms.common.SignInButton signInButton;
-
-    // TextView to Show Login User Email and Name.
-    TextView LoginUserName, LoginUserEmail;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        Toasty.success(getBaseContext(), "Welcome to TechnoSpandan Gallery.", Toast.LENGTH_LONG, true).show();
 
 
-        signInButton = findViewById(R.id.sign_in_button);
-
-        signInButton = findViewById(R.id.sign_in_button);
-
-        // Getting Firebase Auth Instance into firebaseAuth object.
-        firebaseAuth = FirebaseAuth.getInstance();
-        // Creating and Configuring Google Sign In object.
-        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-
-        // Creating and Configuring Google Api Client.
-        googleApiClient = new GoogleApiClient.Builder(SignInActivity.this)
-                .enableAutoManage(SignInActivity.this , new GoogleApiClient.OnConnectionFailedListener() {
-                    @Override
-                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-                    }
-                } /* OnConnectionFailedListener */)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOptions)
-                .build();
-
-
-        // Adding Click listener to User Sign in Google button.
-        signInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                UserSignInMethod();
-
-            }
-        });}
-
-
-    // Sign In function Starts From Here.
-    public void UserSignInMethod(){
-
-        // Passing Google Api Client into Intent.
-        Intent AuthIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
-
-        startActivityForResult(AuthIntent, RequestSignInCode);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == RequestSignInCode){
-
-            GoogleSignInResult googleSignInResult = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-
-            if (googleSignInResult.isSuccess()){
-
-                Toasty.success(getBaseContext(), "SignIn Successfull...", Toast.LENGTH_SHORT, true).show();
-
-
-                GoogleSignInAccount acct= GoogleSignIn.getLastSignedInAccount(this);
-                if(acct!=null){
-                    String personName=acct.getDisplayName();
-                    Toasty.info(getBaseContext(), "Welcome "+ personName, Toast.LENGTH_LONG, true).show();
-                }
-
-
-               Intent intent = new Intent(this, ImgUploadActivity.class);
-                startActivity(intent);
-
-
-            }
-
-        }
-    }
 
 }
+
+    public void buttonOn(View view){
+        switch(view.getId()) {
+            case R.id.button5:
+                Intent myIndent = new Intent(this, ImgUploadActivity.class);
+                startActivity(myIndent);
+                break;
+        } }}
